565f053423ab6f212ca058c77a8d7aa902a9bb72
Eli Cohen <elicohen@Elis-MacBook-Pro.local>
2018-04-26 18:37:07 +0300

    test commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
   Task = require('./api/models/todoListModel'), //created model loading here
   bodyParser = require('body-parser');
 
-// mongoose instance connection url connection
+/// mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://ecmongodb:g9VEPWmjgi0HCodUcOrkFftV1W8qu1v77QKAkzoLIGZyXMf9XJSWEg2W29hPvihIVipKuCFd9zhP02jyAq4cbQ==@ecmongodb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb/Tododb');
 
